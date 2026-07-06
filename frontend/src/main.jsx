2af4264c9764193852512c6cfc1a9a09e209a371
suyashsinghx <suyashsinghx@gmail.com>
2026-07-06 23:21:52 +0530

    add Clerk authentication integration and update dependencies

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+
+import { ClerkProvider } from "@clerk/react";   //Clerk for authentication
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+  <ClerkProvider>
     <App />
+  </ClerkProvider>
   </StrictMode>,
 )
